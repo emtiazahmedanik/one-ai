e00af14971c209cb0e62e02b1d758afbc9614fd4
emtiazahmed <emtiazahmedanik4@gmail.com>
2025-08-08 14:52:54 +0600

    done

---
+++ lib/constants/ai_list.dart
@@ -0,0 +1,90 @@
+class AiList{
+
+  static const Map<String, String> tagMap = {
+    'All': 'All',
+    'Ai Assistant': 'Assistant',
+    'Ai Writing': 'Writing',
+
+  };
+
+  static const List<Map<String, dynamic>> aiList = [
+    {
+      'imagePath': 'assets/images/chatgpt.png',
+      'title': 'ChatGpt',
+      'url': 'https://chatgpt.com/',
+      'tag': 'Assistant'
+    },
+    {
+      'imagePath': 'assets/images/gemini.png',
+      'title': 'Gemini',
+      'url': 'https://gemini.google.com/app',
+      'tag': 'Assistant'
+    },
+    {
+      'imagePath': 'assets/images/perplexity.webp',
+      'title': 'Perplexity',
+      'url': 'https://www.perplexity.ai/',
+      'tag': 'Assistant'
+    },
+    {
+      'imagePath': 'assets/images/grok.webp',
+      'title': 'Grok',
+      'url': 'https://grok.com/',
+      'tag': 'Assistant'
+    },
+    {
+      'imagePath': 'assets/images/deepseek.webp',
+      'title': 'DeepSeek ',
+      'url': 'https://chat.deepseek.com/',
+      'tag': 'Assistant'
+    },
+    {
+      'imagePath': 'assets/images/mistral.webp',
+      'title': 'Mistral ',
+      'url': 'https://chat.mistral.ai/chat',
+      'tag': 'Assistant'
+    },
+    {
+      'imagePath': 'assets/images/copilot.webp',
+      'title': 'Copilot ',
+      'url': 'https://copilot.microsoft.com/chats',
+      'tag': 'Assistant'
+    },
+    {
+      'imagePath': 'assets/images/blackbox.png',
+      'title': 'BlackBox ',
+      'url': 'https://www.blackbox.ai/',
+      'tag': 'Assistant'
+    },
+    {
+      'imagePath': 'assets/images/chatbase.png',
+      'title': 'Chatbase',
+      'url': 'https://www.chatbase.co/',
+      'tag': 'Assistant'
+    },
+    {
+      'imagePath': 'assets/images/poe.webp',
+      'title': 'Poe',
+      'url': 'https://poe.com/',
+      'tag': 'Assistant'
+    },
+    {
+      'imagePath': 'assets/images/quillbot.png',
+      'title': 'Quillbot ',
+      'url': 'https://quillbot.com/',
+      'tag': 'Writing'
+    },
+    {
+      'imagePath': 'assets/images/grammarly.png',
+      'title': 'Grammarly ',
+      'url': 'https://app.grammarly.com/',
+      'tag': 'Writing'
+    },
+    {
+      'imagePath': 'assets/images/copyai.png',
+      'title': 'Copy.ai ',
+      'url': 'https://app.copy.ai/',
+      'tag': 'Writing'
+    },
+  ];
+}
